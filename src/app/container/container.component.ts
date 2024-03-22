64060b59df18a8d64b2808366efb852e08c66a48
dinhvuhcmus <dinhvuhcmus@gmail.com>
2024-03-22 10:21:41 +0700

    Init project. Learn fundamental Angular and develop some components as Header, Product, ProductList, Search, Filter

---
+++ src/app/container/container.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.css'
+})
+export class ContainerComponent {
+  // name: string = "Vu";
+
+  product = {
+    name: "Iphone X",
+    price: 100,
+    color: "black",
+    discountedPrice: 10,
+    inStock: 10,
+    pImage: "/assets/images/iphone-14-128.jpg"
+  }
+
+  
+  getDiscountedPrice() {
+    return this.product.price - ((this.product.price * this.product.discountedPrice) / 100)
+  }
+  
+  addToCartNumber: number = 0;
+
+  decrementCartValue() {
+    if (this.addToCartNumber > 0) {
+      this.addToCartNumber--;
+    }
+  }
+
+  incrementCartValue() {
+    if(this.addToCartNumber < this.product.inStock) {
+      this.addToCartNumber++;
+    }
+  }
+
+  searchText: string = '';
+
+  setSearchText(value: string) {
+    this.searchText = value;
+  }
+
+
+  // onHandleInput(event: any) {
+  //   this.name = event.target.value;
+  // }
+}
---
+++ src/app/container/search/search.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+
+  searchText: string = "";
+
+  onSearchText(event: any) {
+    this.searchText = event.target.value;
+  }
+
+  @Output()
+  searchTextChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  onSearchTextChanged() {
+    this.searchTextChanged.emit(this.searchText);
+  }
+
+}

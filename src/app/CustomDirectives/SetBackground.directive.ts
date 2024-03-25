d6a43b250f2eb52f5a278291b11d54b321203b0d
dinhvuhcmus <dinhvuhcmus@gmail.com>
2024-03-25 17:04:10 +0700

    Should use Renderer2 in order not to access directly the DOM elements

--- src/app/CustomDirectives/SetBackground.directive.ts
+++ src/app/CustomDirectives/SetBackground.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from "@angular/core";
+import { Directive, ElementRef, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[setBackground]'
@@ -8,15 +8,24 @@ export class SetBackground {
 
     // We need to create a property here because the ngOnInit can not access directly the element reference.
     // private element: ElementRef; 
+    // private renderer: Renderer2;
 
-    constructor(private element: ElementRef) {
+    constructor(private element: ElementRef, private renderer: Renderer2) {
         // this.element = element;
+        // this.renderer = renderer;
     }
 
     // Note: implement code in ngOnInit because the ngOnInit hook is called after all component properties are fully initialized.
     ngOnInit() {
-        this.element.nativeElement.style.backgroundColor = "#36454F";
-        this.element.nativeElement.style.color = "white";
+        // We should nto access the DOM element directly. => Should using Renderer2.
+        // this.element.nativeElement.style.backgroundColor = "#36454F";
+        // this.element.nativeElement.style.color = "white";
+
+        //=> Should using Renderer2.
+        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', '#36454F');
+        this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
     }
+
+
 
 }
